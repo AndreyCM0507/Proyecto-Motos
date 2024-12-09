13c2252fac89a7a49c94690d1194292578c4bce9
AndreyCM0507 <andreycmina0507@gmail.com>
2024-12-08 22:29:13 -0600

    Add files via upload

---
+++ ProyectoMotos/js/Java.js
@@ -0,0 +1,32 @@
+let btnKawasaki500 = document.getElementById("btnKawa500");
+
+btnKawasaki500.addEventListener('click',()=>{
+    window.open('https://www.kawasaki.es/es/products/Supersport/2024/Ninja_500_SE/overview?Uid=08D3Dg5fDA1cXF4JCwwMXFxfX1FcXAoNXVFfDA5YWltdXlg','_blank')
+})
+
+let btnBMWF800 = document.querySelector('#btnBMWF800');
+btnBMWF800.addEventListener("click",()=>{
+    window.open('https://www.bmw-motorrad.cr/es/models/adventure/f800gs.html','_blank')
+})
+
+let btnFZ150 = document.querySelector('#btnFZ150');
+btnFZ150.addEventListener('click', ()=>{
+    window.open('https://www.yamahacostarica.com/product-page/fz150-s-versi%C3%B3n-2-0','_blank')
+})
+
+
+let btnKawaNinjaZX10R = document.querySelector('#btnKawaNinjaZX10R');
+btnKawaNinjaZX10R.addEventListener('click', ()=>{
+    window.open('https://costarica.kawasaki-la.com/es-la/motocicleta/ninja/supersport/ninja-zx-10r','_blank')
+});
+
+let btnYamahar3 = document.getElementById('btnYamahar3');
+btnYamahar3.addEventListener('click',()=>{
+    window.open('https://www.yamahacostarica.com/product-page/yzf-r3-1','_blank')
+});
+
+let btnBMW100RR = document.getElementById('btnBMW100RR');
+
+btnBMW100RR.addEventListener("click",()=>{
+    window.open('https://www.bmw-motorrad.cr/es/models/m/m1000rr.html','_blank');
+});
